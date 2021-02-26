74fb187694d844b9c87ff26cba0836fe4e0cb2a3
Thanayut T <b@thanayut.in.th>
2021-02-26 16:12:17 +0000

    completed display slides function

--- display.py
+++ display.py
@@ -29,7 +29,25 @@ def begin():
         if q[0] == 0:
             return render_template("display/error.html", msg="Signage ID not found.")
         else:
-            session["signageid"] = request.form.get("signageid")
+            session["signageid"] = sid
             return redirect("/display/show")
     else:
         return redirect("/display")
+
+
+@display.route("show")
+def show():
+    if not session["signageid"]:
+        return redirect("/display")
+
+    # Query for slides
+    conn = sqlite3.connect(dbpath)
+    c = conn.cursor()
+    c.execute("SELECT imageurl, dduration FROM slides WHERE signageid=? ORDER BY iindex ASC", (session["signageid"],))
+    slides = c.fetchall()
+    conn.close()
+
+    if len(slides) < 1:
+        return render_template("display/error.html", msg="Signage doesn't contain any slide.")
+
+    return render_template("display/show.html", signageid=session["signageid"], slides=slides)
