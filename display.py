ff8790345f98865a84820cc537acd4302a6134d2
Thanayut T <b@thanayut.in.th>
2021-02-26 11:50:03 +0000

    added signageid validation in display

--- display.py
+++ display.py
@@ -1,6 +1,10 @@
-from flask import Blueprint, render_template, request, redirect
+from flask import Blueprint, render_template, request, redirect, session
+import sqlite3
 
 display = Blueprint("display", __name__)
+
+# Config db connection
+dbpath = "database.db"
 
 @display.route("")
 def index():
@@ -14,7 +18,16 @@ def begin():
             return render_template("display/error.html", msg="Please enter a Signage ID.")
 
         # Check for Signage ID in database
-        # if found display
-        # if not error
+        sid = request.form.get("signageid").upper()
+        conn = sqlite3.connect(dbpath)
+        c = conn.cursor()
+        c.execute("SELECT COUNT(signageid) AS \"COUNT(signageid)\" FROM signages WHERE signageid=?", (sid,))
+        q = c.fetchone()
+
+        if q[0] == 0:
+            return render_template("display/error.html", msg="Signage ID not found.")
+        else:
+            session["signageid"] = request.form.get("signageid")
+            return redirect("/display/show")
     else:
         return redirect("/display")
