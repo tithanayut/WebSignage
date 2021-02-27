c807b1f41b4a706f8164b24a9a5474316055d6e8
Thanayut T <b@thanayut.in.th>
2021-02-27 04:56:54 +0000

    added password auth for signage

--- display.py
+++ display.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import Blueprint, render_template, request, redirect, session
+from werkzeug.security import check_password_hash, generate_password_hash
 import sqlite3
 
 display = Blueprint("display", __name__)
@@ -16,38 +17,49 @@ def index():
 @display.route("begin", methods=["GET", "POST"])
 def begin():
     if request.method == "POST":
-        if not request.form.get("signageid"):
-            return render_template("display/error.html", msg="Please enter a Signage ID.")
+        if not request.form.get("signageid") or not request.form.get("signagepw"):
+            return render_template("display/error.html", msg="Please enter a Signage ID and password.")
 
         # Check for Signage ID in database
         sid = request.form.get("signageid").upper()
         conn = sqlite3.connect(dbpath)
         c = conn.cursor()
-        c.execute("SELECT COUNT(signageid) AS \"COUNT(signageid)\" FROM signages WHERE signageid=?", (sid,))
+        c.execute("SELECT COUNT(signageid) AS \"COUNT(signageid)\", password FROM signages WHERE signageid=?", (sid,))
         q = c.fetchone()
+        conn.close()
 
         if q[0] == 0:
             return render_template("display/error.html", msg="Signage ID not found.")
+
+        if q[1] is not None:
+            if not check_password_hash(q[1], request.form.get("signagepw")):
+                return render_template("display/error.html", msg="Incorrect password.")
         else:
-            session["signageid"] = sid
-            return redirect("/display/show")
+            return render_template("display/error.html", msg="Signage could not be displayed.")
+
+        session["signageid"] = sid
+        return redirect("/display/show")
     else:
         return redirect("/display")
 
 
 @display.route("show")
 def show():
-    if not session["signageid"]:
+    if session.get("signageid") is None:
         return redirect("/display")
+    sid = session["signageid"]
+
+    # Pop signageid session
+    session.pop("signageid")
 
     # Query for slides
     conn = sqlite3.connect(dbpath)
     c = conn.cursor()
-    c.execute("SELECT imageurl, dduration FROM slides WHERE signageid=? ORDER BY iindex ASC", (session["signageid"],))
+    c.execute("SELECT imageurl, dduration FROM slides WHERE signageid=? ORDER BY iindex ASC", (sid,))
     slides = c.fetchall()
     conn.close()
 
     if len(slides) < 1:
         return render_template("display/error.html", msg="Signage doesn't contain any slide.")
 
-    return render_template("display/show.html", signageid=session["signageid"], slides=slides)
+    return render_template("display/show.html", signageid=sid, slides=slides)
